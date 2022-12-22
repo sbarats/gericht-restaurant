cf3c58444db9b0455614cf745aaf3d8b3ffcad14
Sara <sara.a.barats@gmail.com>
2022-12-21 21:26:26 -0500

    navbar and header

--- src/App.js
+++ src/App.js
@@ -10,26 +10,23 @@ import {
   Intro,
   Laurels,
   SpecialMenu,
-  FindUs,
 } from "./container";
 import { Navbar } from "./components";
 import "./App.css";
 
-const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <Header />
-      <AboutUs />
-      <SpecialMenu />
-      <Chef />
-      <Intro />
-      <Laurels />
-      <Gallery />
-      <Contact />
-      <Footer />
-    </div>
-  );
-};
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <Contact />
+    <Footer />
+  </div>
+);
 
 export default App;
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 
+// import { images } from '../../constants';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper-info">
+      <SubHeading title="The Pinnacle of Fine Dining"/>
+      <h1 className="app__header-h1">The Pinnacle of Fine Dining</h1>
+      <p className="p__opensans">Test</p>
+      <button type="button" className="header__button">View Menu</button>
+    </div>
+    <div className="app__header-img">
+      <img src={images.welcome} alt="Featured dish: Baked Salmon"/>
+    </div>
   </div>
 );
 
